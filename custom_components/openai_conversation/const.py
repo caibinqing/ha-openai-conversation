53a545783008378205c03a168eb4320ece3a281d
Necroneco <caibinqing@live.com>
2025-02-06 13:40:44 +0800

    HA 2025.2.0

--- custom_components/openai_conversation/const.py
+++ custom_components/openai_conversation/const.py
@@ -5,6 +5,8 @@ import logging
 DOMAIN = "openai_conversation"
 LOGGER = logging.getLogger(__package__)
 
+CONF_BASE_URL = "base_url"
+CONF_PROXY = "proxy"
 CONF_RECOMMENDED = "recommended"
 CONF_PROMPT = "prompt"
 CONF_CHAT_MODEL = "chat_model"
@@ -15,5 +17,3 @@ CONF_TOP_P = "top_p"
 RECOMMENDED_TOP_P = 1.0
 CONF_TEMPERATURE = "temperature"
 RECOMMENDED_TEMPERATURE = 1.0
-CONF_BASE_URL = "base_url"
-CONF_PROXY = "proxy"

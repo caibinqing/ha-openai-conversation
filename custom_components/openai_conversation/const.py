6c700bdbffa06bde8759b772697e9cb106881d79
Necroneco <caibinqing@live.com>
2025-07-04 11:10:49 +0800

    HA 2025.7.0

--- custom_components/openai_conversation/const.py
+++ custom_components/openai_conversation/const.py
@@ -5,11 +5,13 @@ import logging
 DOMAIN = "openai_conversation"
 LOGGER: logging.Logger = logging.getLogger(__package__)
 
+DEFAULT_CONVERSATION_NAME = "OpenAI Conversation"
+DEFAULT_NAME = "OpenAI Conversation"
+
 CONF_BASE_URL = "base_url"
 CONF_CHAT_MODEL = "chat_model"
 CONF_FILENAMES = "filenames"
 CONF_MAX_TOKENS = "max_tokens"
-CONF_PROMPT = "prompt"
 CONF_PROMPT = "prompt"
 CONF_PROXY = "proxy"
 CONF_REASONING_EFFORT = "reasoning_effort"
